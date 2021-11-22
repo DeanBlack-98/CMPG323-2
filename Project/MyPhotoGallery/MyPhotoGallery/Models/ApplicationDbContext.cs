7ad41513eea997d251b2dcd7de731d65733999ed
MARIUS SWART <dswart54@yahoo.com>
2021-11-22 23:51:10 +0200

    Adding of failures zip

---
+++ Project/MyPhotoGallery/MyPhotoGallery/Models/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace MyPhotoGallery.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<GalleryImage> GalleryImages { get; set; }
+        public DbSet<ImageTag> ImageTags { get; set; }
+    }
+}
---
+++ Project/MyPhotoGallery/MyPhotoGallery/Models/GalleryImage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyPhotoGallery.Models
+{
+    public class GalleryImage
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public DateTime Created { get; set; }
+        public string URL { get; set; }
+        public virtual IEnumerable<ImageTag> Tags { get; set; }
+    }
+}
